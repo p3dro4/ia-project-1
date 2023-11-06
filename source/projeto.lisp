cf3080c97e05dd3612d42e8db2da48d536f44ab8
p3dro4 <202100230@estudantes.ips.pt>
2023-11-06 16:30:39 +0000

    Move source code files to source folder and Add description to files

---
+++ source/projeto.lisp
@@ -0,0 +1,3 @@
+;;;; projeto.lisp
+;;;; Carrega os outros ficheiros de código, escreve e lê ficheiros, e trata da interação com o utilizador.
+;;;; Autores: 202100230 - Pedro Anjos, 202100225 - André Meseiro
---
+++ source/puzzle.lisp
@@ -0,0 +1,3 @@
+;;;; puzzle.lisp
+;;;; Código relacionado com o problema.   
+;;;; Autores: 202100230 - Pedro Anjos, 202100225 - André Meseiro
