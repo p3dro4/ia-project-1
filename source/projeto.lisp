7bd0a7c36ee08a0185c68cbce6253a10cc67e35a
p3dro4 <202100230@estudantes.ips.pt>
2023-11-16 16:24:28 +0000

    Add loading of project .lisp files
    This allows the user to load the all project files only by loading project.lisp

--- source/projeto.lisp
+++ source/projeto.lisp
@@ -1,3 +1,17 @@
 ;;;; projeto.lisp
 ;;;; Carrega os outros ficheiros de código, escreve e lê ficheiros, e trata da interação com o utilizador.
 ;;;; Autores: 202100230 - Pedro Anjos, 202100225 - André Meseiro
+(defun carregar-ficheiros ()
+  (format t ";; A carregar ficheiros...~%")
+  (cond ((null *load-pathname*) nil)
+        (t (let ((load-path *load-pathname*))
+              (load (merge-pathnames "procura" load-path))
+              (load (merge-pathnames "puzzle" load-path)))))
+  (cond ((null *compile-file-pathname*) nil)
+        (t (let ((load-path *compile-file-pathname*))
+              (load (merge-pathnames "procura" load-path))
+              (load (merge-pathnames "puzzle" load-path)))))
+  (format t ";; Ficheiros carregados.~%")
+)
+
+(carregar-ficheiros)
